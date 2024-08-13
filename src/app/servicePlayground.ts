4dd81b61edce79f8d7a612620816cf8b3f32c7b5
Vaibhav Gandhi <vaibhavgandhi1801@gmail.com>
2024-08-13 02:54:08 -0700

    Added transcript functionality

--- src/app/servicePlayground.ts
+++ src/app/servicePlayground.ts
@@ -3,7 +3,11 @@ import { Store } from "@ngrx/store";
 import { animate, style, transition, trigger } from "@angular/animations";
 import { Router } from "@angular/router";
 import { AIDataService } from "./service/aidata.service";
-
+interface Message {
+  sender: string;
+  message: string;
+  interval: number;
+}
 @Component({
   moduleId: module.id,
   templateUrl: "./servicePlayground.html",
@@ -29,6 +33,18 @@ export class ServicePlaygroundComponent implements OnInit, OnDestroy {
   callStatus = "";
 
   userPrompt = "";
+  messages: Message[] = [
+    { sender: "User", message: "Hi there!", interval: 1000 },
+    { sender: "Bot", message: "Hello! How can I help you?", interval: 2000 },
+    { sender: "User", message: "I need some assistance.", interval: 1500 },
+    {
+      sender: "Bot",
+      message: "Sure, what do you need help with?",
+      interval: 2500,
+    },
+  ];
+  displayedMessages: { sender: string; displayedText: string }[] = [];
+  private currentMessageIndex: number = 0;
   constructor(
     public storeData: Store<any>,
     private router: Router,
@@ -66,7 +82,41 @@ export class ServicePlaygroundComponent implements OnInit, OnDestroy {
         }
       });
   }
+  displayNextMessage(): void {
+    if (this.currentMessageIndex < this.messages.length) {
+      const message = this.messages[this.currentMessageIndex];
+      this.typeMessage(message);
+    } else {
+      this.clearTimer();
+    }
+  }
 
+  typeMessage(message: Message): void {
+    let currentIndex = 0;
+    let displayedText = "";
+    const typingSpeed = 120; // Adjust typing speed as needed
+
+    const typeInterval = setInterval(() => {
+      displayedText += message.message.charAt(currentIndex);
+      this.updateDisplayedMessage(message.sender, displayedText);
+      currentIndex++;
+
+      if (currentIndex === message.message.length) {
+        clearInterval(typeInterval);
+        this.currentMessageIndex++;
+        setTimeout(() => this.displayNextMessage(), message.interval);
+      }
+    }, typingSpeed);
+  }
+
+  updateDisplayedMessage(sender: string, displayedText: string): void {
+    if (this.displayedMessages[this.currentMessageIndex]) {
+      this.displayedMessages[this.currentMessageIndex].displayedText =
+        displayedText;
+    } else {
+      this.displayedMessages.push({ sender, displayedText });
+    }
+  }
   userAgentPost() {
     // this.aiDataService.userAgentCall(4254926201, this.userPrompt).subscribe((response) => {
     //     console.log(response);
@@ -74,6 +124,7 @@ export class ServicePlaygroundComponent implements OnInit, OnDestroy {
   }
   startTimer(): void {
     this.callStatus = "started";
+    this.displayNextMessage();
     this.intervalId = setInterval(() => {
       this.seconds++;
 
